fe85d90e3b654262253ada3a45dd84b5ea3aac4a
Cecilia Edstein <cecilia.edstein@gmail.com>
2026-01-06 15:37:53 +0100

    hero ts to main ts

--- src/Ts/hero.ts
+++
@@ -1,9 +0,0 @@
-export const heroButton = document.getElementById(
-  "hero-cta"
-) as HTMLButtonElement | null;
-
-if (heroButton) {
-  heroButton.addEventListener("click", () => {
-    window.location.href = "/oneproduct.html";
-  });
-}
