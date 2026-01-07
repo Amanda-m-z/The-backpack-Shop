929ad35949b9f4c228b4f5e2bec0bee08b223c63
Cecilia Edstein <cecilia.edstein@gmail.com>
2026-01-07 15:45:34 +0100

    conflict two solved

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { loadHeader } from "./Ts/loadHeader";
 import "./scss/style.scss";
 const heroButton = document.getElementById("heroCta");
 
-<<<<<<< HEAD
 if (heroButton) {
   heroButton.addEventListener("click", () => {
     window.location.href = "/oneproduct.html";
@@ -17,8 +16,6 @@ if (header) {
 } else {
   console.error("Header element with the id 'headerContainer' not found");
 }
-=======
 heroButton?.addEventListener("click", () => {
   window.location.href = "/oneproduct.html";
 });
->>>>>>> feature-Hero-img
