e7c264fdac911f66ac9ac55868ecb708513f1f1d
Amanda-m-z <amanda.z.mickaela@gmail.com>
2026-01-07 09:37:44 +0100

    added style

--- src/allProducts.ts
+++ src/allProducts.ts
@@ -4,27 +4,27 @@ export const allProducts: Backpack[] = [];
 
 
 //Frilufts-ryggsäckar
-allProducts.push(new Backpack("Friluft", "1099", 20, "Friluft 1", "backpacks/friluft/friluft1.webp", "mint"));
-allProducts.push(new Backpack("Friluft", "1099", 20, "Friluft 2", "backpacks/friluft/friluft2.webp", "green"));
-allProducts.push(new Backpack("Friluft", "1099", 20, "Friluft 3", "backpacks/friluft/friluft3.webp", "red"));
-allProducts.push(new Backpack("Friluft", "1099", 20, "Friluft 4", "backpacks/friluft/friluft4.webp", "orange"));
-allProducts.push(new Backpack("Friluft", "1099", 20, "Friluft 5", "backpacks/friluft/friluft5.webp", "beige"));
-allProducts.push(new Backpack("Friluft", "1099", 20, "Friluft 6", "backpacks/friluft/friluft6.webp", "purple"));
+allProducts.push(new Backpack("Friluft", "1099", 20, "Kånken - Mint", "backpacks/friluft/friluft1.webp", "mint"));
+allProducts.push(new Backpack("Friluft", "1099", 20, "Kånken - Green", "backpacks/friluft/friluft2.webp", "green"));
+allProducts.push(new Backpack("Friluft", "1099", 20, "Kånken - Red", "backpacks/friluft/friluft3.webp", "red"));
+allProducts.push(new Backpack("Friluft", "1099", 20, "Kånken - Orange", "backpacks/friluft/friluft4.webp", "orange"));
+allProducts.push(new Backpack("Friluft", "1099", 20, "Kånken - Beige", "backpacks/friluft/friluft5.webp", "beige"));
+allProducts.push(new Backpack("Friluft", "1099", 20, "Kånken - Purple", "backpacks/friluft/friluft6.webp", "purple"));
 
 
 //Laptops-ryggsäckar
-allProducts.push(new Backpack("Laptop", "999", 30, "Laptop 1", "backpacks/laptop/laptop1.jpg", "beige"));
-allProducts.push(new Backpack("Laptop", "999", 30, "Laptop 2", "backpacks/laptop/laptop2.jpg", "green"));
-allProducts.push(new Backpack("Laptop", "999", 30, "Laptop 3", "backpacks/laptop/laptop3.jpg", "black"));
-allProducts.push(new Backpack("Laptop", "999", 30, "Laptop 4", "backpacks/laptop/laptop4.jpg", "blue"));
-allProducts.push(new Backpack("Laptop", "999", 30, "Laptop 5", "backpacks/laptop/laptop5.jpg", "grey"));
-allProducts.push(new Backpack("Laptop", "999", 30, "Laptop 6", "backpacks/laptop/laptop6.jpg", "pink"));
+allProducts.push(new Backpack("Laptop", "999", 30, "Nordace Siena - Beige", "backpacks/laptop/laptop1.jpg", "beige"));
+allProducts.push(new Backpack("Laptop", "999", 30, "Nordace Siena - Green", "backpacks/laptop/laptop2.jpg", "green"));
+allProducts.push(new Backpack("Laptop", "999", 30, "Nordace Siena - Black", "backpacks/laptop/laptop3.jpg", "black"));
+allProducts.push(new Backpack("Laptop", "999", 30, "Nordace Siena - Blue", "backpacks/laptop/laptop4.jpg", "blue"));
+allProducts.push(new Backpack("Laptop", "999", 30, "Nordace Siena - Grey", "backpacks/laptop/laptop5.jpg", "grey"));
+allProducts.push(new Backpack("Laptop", "999", 30, "Nordace Siena - Pink", "backpacks/laptop/laptop6.jpg", "pink"));
 
 
 //Vardags-ryggsäckar
-allProducts.push(new Backpack("Everyday", "899", 40, "Everyday 1", "backpacks/vardags/everyday1.webp", "black"));
-allProducts.push(new Backpack("Everyday", "899", 40, "Everyday 2", "backpacks/vardags/everyday2.webp", "navy"));
-allProducts.push(new Backpack("Everyday", "899", 40, "Everyday 3", "backpacks/vardags/everyday3.webp", "white"));
-allProducts.push(new Backpack("Everyday", "899", 40, "Everyday 4", "backpacks/vardags/everyday4.webp", "blue"));
-allProducts.push(new Backpack("Everyday", "899", 40, "Everyday 5", "backpacks/vardags/everyday5.webp", "beige"));
-allProducts.push(new Backpack("Everyday", "899", 40, "Everyday 6", "backpacks/vardags/everyday6.webp", "red"));
+allProducts.push(new Backpack("Everyday", "899", 40, "New Crew Backpack - Black", "backpacks/vardags/everyday1.webp", "black"));
+allProducts.push(new Backpack("Everyday", "899", 40, "New Crew Backpack - Navy", "backpacks/vardags/everyday2.webp", "navy"));
+allProducts.push(new Backpack("Everyday", "899", 40, "New Crew Backpack - White", "backpacks/vardags/everyday3.webp", "white"));
+allProducts.push(new Backpack("Everyday", "899", 40, "New Crew Backpack - Blue", "backpacks/vardags/everyday4.webp", "blue"));
+allProducts.push(new Backpack("Everyday", "899", 40, "New Crew Backpack - Beige", "backpacks/vardags/everyday5.webp", "beige"));
+allProducts.push(new Backpack("Everyday", "899", 40, "New Crew Backpack - Red", "backpacks/vardags/everyday6.webp", "red"));
