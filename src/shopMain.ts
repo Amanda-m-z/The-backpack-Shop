48c2297775d1f19f0e2349e95d2dff9997e960b5
Amanda-m-z <amanda.z.mickaela@gmail.com>
2026-01-08 16:23:12 +0100

    added pages map

---
+++ src/shopMain.ts
@@ -0,0 +1,8 @@
+import { createHTMLOneProduct } from "./OneProduct/productOneView";
+
+createHTMLOneProduct();
+
+
+const oneProductDiv = document.getElementById("oneProductView");
+
+createHTMLOneProduct();
