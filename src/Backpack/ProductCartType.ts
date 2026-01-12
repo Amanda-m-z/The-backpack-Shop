987b1f8ff9455e024788e07131a472dae2795b28
Amanda-m-z <amanda.z.mickaela@gmail.com>
2026-01-12 17:33:15 +0100

    added new property to json file

--- src/Backpack/ProductCartType.ts
+++ src/Backpack/ProductCartType.ts
@@ -6,6 +6,7 @@ export type ProductCart = {
     img: string;
     color: string;
     id: number;
-    quantity: number;  
+    quantity: number;
+    freshness: string;  
        
 }
---
+++ src/Ts/loopNew.ts
@@ -0,0 +1,46 @@
+import type { Product } from "../Backpack/ProductType";
+import { getProduct } from "../getProduct";
+
+
+export const loopNew = async (): Promise<void> => {
+
+  const listNew: Product[] = await getProduct();
+  const div = document.getElementById("newDiv");
+
+  listNew.forEach((item) => {
+
+        const productContainer = document.createElement("div");
+        const imgContainer = document.createElement("div");
+        const img = document.createElement("img");
+        const extraInfo = document.createElement("div");
+        const name = document.createElement("h2");
+        const price = document.createElement("p");
+    
+        productContainer.className = "productContainer";
+        imgContainer.className = "imgContainer";
+        imgContainer.id = "imgContainerID";
+        img.src = item.img;
+        extraInfo.className = "extraInfo"
+        name.innerHTML = item.name;
+        name.id = ("nameID");
+        price.innerHTML = item.price+"kr";
+    
+    
+        imgContainer.appendChild(img);
+        productContainer.appendChild(imgContainer);
+        extraInfo.appendChild(name);
+        extraInfo.appendChild(price);
+        productContainer.appendChild(extraInfo);
+        div?.appendChild(productContainer);
+    
+        
+        imgContainer.addEventListener("click", () => {
+         //openOneProduct(item);
+          const theBag = JSON.stringify(item);   //Gör till string
+          localStorage.setItem("TheBag", theBag);   //Spara i localStorage
+          window.location.href = "src/pages/oneproduct.html"; //Öppna oneproduct sida
+        });
+  });
+
+}
+
