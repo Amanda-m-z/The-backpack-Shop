e8f69345e97b317a5106d7c538cdf06f8b5d4ebf
Emma Pang <emmapang3@gmail.com>
2026-01-14 21:27:55 +0100

    added styling for the pop up and added some comments

--- src/Ts/oneProductPage/showAddedToCartPopUp.ts
+++ src/Ts/oneProductPage/showAddedToCartPopUp.ts
@@ -1,5 +1,6 @@
 export function showAddedToCartPopUp() {
   const container = document.getElementById("addedToCartPopUpSection");
+  const popUpContainer = document.createElement("div");
   const textContainer = document.createElement("div");
   const popUpIcon = document.createElement("i");
   const popUpText = document.createElement("p");
@@ -7,18 +8,25 @@ export function showAddedToCartPopUp() {
   const continueShoppingBtn = document.createElement("button");
   const showShoppingCartBtn = document.createElement("button");
 
+  popUpContainer.id = "popUpContainer";
   textContainer.className = "textContainer";
   popUpIcon.className = "fa-regular fa-circle-check";
-  popUpText.textContent = "Varan har lagts till i varukorgen";
+  popUpText.textContent = "Varan har lagts till i varukorgen! :D";
   buttonsContainer.className = "buttonsContainer";
   continueShoppingBtn.textContent = "Fortsätt handla";
-  showShoppingCartBtn.textContent = "Visa varukorgen";
+  continueShoppingBtn.className = "btn btn-secondary";
+  continueShoppingBtn.addEventListener("click", () => {
+    container?.classList.add("close-overlay");
+  });
+  showShoppingCartBtn.textContent = "Gå till kassan";
+  showShoppingCartBtn.className = "btn btn-primary";
 
   textContainer.append(popUpIcon, popUpText);
   buttonsContainer.append(continueShoppingBtn, showShoppingCartBtn);
-  container?.append(textContainer, buttonsContainer);
+  popUpContainer.append(textContainer, buttonsContainer);
+  container?.appendChild(popUpContainer);
 
   setTimeout(() => {
-    container?.classList.toggle("close-overlay");
+    container?.classList.add("close-overlay");
   }, 4000);
 }
