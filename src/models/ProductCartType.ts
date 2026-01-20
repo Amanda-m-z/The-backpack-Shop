d17383bad3b01d6d76b701502e38c30bcd0e0f58
Amanda-m-z <amanda.z.mickaela@gmail.com>
2026-01-20 10:54:48 +0100

    removed product cart purschases

--- src/models/ProductCartType.ts
+++ src/models/ProductCartType.ts
@@ -1,11 +1,9 @@
 export type ProductCart = {
     type: string;
     price: number;
-    purchases: number;  //OBS! Ska denna vara med här?
     name: string;
     img: string;
     color: string;
     id: number;
-    quantity: number;
-       
+    quantity: number;     
 }
