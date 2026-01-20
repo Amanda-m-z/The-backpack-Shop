f296d97213f05a92493147ed5b2c5e992f59e47d
Emma Pang <emmapang3@gmail.com>
2026-01-20 11:08:32 +0100

    added date and time for each product in the JSON-file, made most popular products the default view for the shop page and added an eventlistener for when the user changes the option of the sort button that will update the order of the products in the array and on the page

--- src/pages/Shop/shopMain.ts
+++ src/pages/Shop/shopMain.ts
@@ -1,23 +1,22 @@
-import '../../scss/style.scss'; 
+import "../../scss/style.scss";
 import { createHTML } from "./createHTMLShop";
 
-import type { Product } from '../../models/ProductType';
-import {loadProductHero } from '../../Ts/productHero';
-import { getProduct } from '../../Services/getProduct';
+import type { Product } from "../../models/ProductType";
+import { loadProductHero } from "../../Ts/productHero";
+import { getProduct } from "../../Services/getProduct";
 
-
-//Detta är för produksidan med alla produkter = shop.html 
+//Detta är för produksidan med alla produkter = shop.html
 
 const theProducts: Product[] = await getProduct();
 
 const productHeroSection = document.getElementById("product-hero");
-if (productHeroSection)
-  loadProductHero();
+if (productHeroSection) loadProductHero();
 
- //const GridDiv = document.getElementById("products-grid");
+//const GridDiv = document.getElementById("products-grid");
 
 //console.log("Detta är resultatet av din fetch", theProducts);
 
-createHTML(theProducts);   //AKTIVERA SENARE
+// Mest populära produkter är default när användaren först kommer in på shoppingsidan
+theProducts.sort((a: Product, b: Product) => b.purchases - a.purchases);
 
-
+createHTML(theProducts); //AKTIVERA SENARE
--- src/pages/Shop/sortProducts.ts
+++ src/pages/Shop/sortProducts.ts
@@ -1,5 +1,13 @@
-export function sortProducts() {
-  const categoriesArray = [
+import type { Product } from "../../models/ProductType";
+import { getProduct } from "../../Services/getProduct";
+import { createHTML } from "./createHTMLShop";
+
+export async function sortProducts() {
+  // Get all the products
+  const theProducts: Product[] = await getProduct();
+
+  // Array for all the options that the user can choose to sort by
+  const sortOptionsArray = [
     {
       title: "Mest populära",
       value: "popular",
@@ -26,13 +34,66 @@ export function sortProducts() {
     },
   ];
 
-  const sortBtn = document.getElementById("sortProductsBtn");
+  const sortBtn = document.getElementById(
+    "sortProductsBtn",
+  ) as HTMLSelectElement;
 
-  categoriesArray.forEach((category) => {
-    const categoryTitle = document.createElement("option");
-    categoryTitle.innerHTML = category.title;
-    categoryTitle.value = category.value;
+  // Creates <option> that will be inside of the <select>
+  sortOptionsArray.forEach((category) => {
+    const option = document.createElement("option");
+    option.innerHTML = category.title;
+    option.value = category.value;
+    sortBtn?.appendChild(option);
+  });
 
-    sortBtn?.appendChild(categoryTitle);
+  // Event "change" handles sorting when the user changes the sort option
+  sortBtn.addEventListener("change", () => {
+    // Gets the value from the option that the user selected
+    const selectedOption = sortBtn.value;
+    // Copy theProducts array
+    const productsCopy = [...theProducts];
+
+    // Switch executes the code block that matches the value of the selected option
+    switch (selectedOption) {
+      // Sorts from most popular to least popular products
+      case "popular":
+        productsCopy.sort(
+          (a: Product, b: Product) => b.purchases - a.purchases,
+        );
+        break;
+
+      // Sorts from newest to oldest
+      case "newest":
+        productsCopy.sort(
+          (a: Product, b: Product) =>
+            new Date(b.dateAdded).getTime() - new Date(a.dateAdded).getTime(),
+        );
+        break;
+
+      // Sorts from cheapest to most expensive
+      case "cheap":
+        productsCopy.sort((a: Product, b: Product) => a.price - b.price);
+        break;
+
+      // Sorts from most expensive to cheapest
+      case "expensive":
+        productsCopy.sort((a: Product, b: Product) => b.price - a.price);
+        break;
+
+      // Sorts A-Ö
+      case "ascendingAlphabet":
+        productsCopy.sort((a: Product, b: Product) =>
+          a.name.localeCompare(b.name, "sv"),
+        );
+        break;
+
+      // Sorts Ö-A
+      case "descendingAlphabet":
+        productsCopy.sort((a: Product, b: Product) =>
+          b.name.localeCompare(a.name, "sv"),
+        );
+        break;
+    }
+    createHTML(productsCopy);
   });
 }
