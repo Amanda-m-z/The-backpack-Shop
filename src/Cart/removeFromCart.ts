2eb8ae2c990c035584732700032f96b229ecb651
Benjamin Palmroth <benjipalmroth@gmail.com>
2026-01-23 14:42:07 +0100

    change map name to lowercase

--- src/Cart/removeFromCart.ts
+++ src/Cart/removeFromCart.ts
@@ -1,27 +1,26 @@
 import type { ProductCart } from "../models/ProductCartType";
-import { createCart } from "./createCart";
+import { createCart } from "../cart/createCart";
 
-export const removeFromCart = (theNewBag: ProductCart) => {  //Denna funtion tar bort en vara från kundkoren
-     
-    let shoppingBag: ProductCart[] = [];
 
-    const theCartString = localStorage.getItem("ShoppingBag")
-    if (theCartString) {
-        shoppingBag = JSON.parse(theCartString);
-    }
+export const removeFromCart = (theNewBag: ProductCart) => {
+  //Denna funtion tar bort en vara från kundkoren
 
-   const existingItem = shoppingBag.find(item => item.id === theNewBag.id)  //Hittar produkten som redan finns 
+  let shoppingBag: ProductCart[] = [];
 
-   if (existingItem && existingItem.quantity > 1 ){  //Om antalet på produkten är större än 1. minskar antal med 1
+  const theCartString = localStorage.getItem("ShoppingBag");
+  if (theCartString) {
+    shoppingBag = JSON.parse(theCartString);
+  }
+
+  const existingItem = shoppingBag.find((item) => item.id === theNewBag.id); //Hittar produkten som redan finns
+
+  if (existingItem && existingItem.quantity > 1) {
+    //Om antalet på produkten är större än 1. minskar antal med 1
     existingItem.quantity--;
-   }
-   else {
-      shoppingBag = shoppingBag.filter(item => item.id !== theNewBag.id); //Filtrerar bort produktem
-   }
+  } else {
+    shoppingBag = shoppingBag.filter((item) => item.id !== theNewBag.id); //Filtrerar bort produktem
+  }
 
-    localStorage.setItem("ShoppingBag", JSON.stringify(shoppingBag)); //Spara ändringar
-    createCart(); //Uppdera kundkorg
-}
-
-
-
+  localStorage.setItem("ShoppingBag", JSON.stringify(shoppingBag)); //Spara ändringar
+  createCart(); //Uppdera kundkorg
+};
